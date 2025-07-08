24b84045a040567df7bf7a60a3b5e5a43c713883
r.williams <richyrichw93@gmail.com>
2025-07-08 16:49:00 +0200

    use webclient builder

--- order_service/src/main/java/com/microservices/order_service/service/OrderService.java
+++ order_service/src/main/java/com/microservices/order_service/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
 
     private final OrderLineItemsMapper orderLineItemsMapper;
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -41,7 +41,7 @@ public class OrderService {
                                      .toList();
         //Call inventory service and place order if product is in stock
         InventoryResponse[] inventoryResponseArray =
-                webClient.get().uri("http://localhost:8082/api/inventory",
+                webClientBuilder.build().get().uri("http://inventory-service/api/inventory",
                                     uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build()).retrieve()
                          .bodyToMono(InventoryResponse[].class).block();
 
